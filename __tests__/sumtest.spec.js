7a45026c74d97ed486e69276cb057e41ef26362c
mjrb <www.w.com123@gmail.com>
2020-05-31 16:36:20 -0400

    fixed thing and added test

--- __tests__/sumtest.spec.js
+++ __tests__/sumtest.spec.js
@@ -4,14 +4,22 @@ const sum = require("../index")
 describe("test sum function", () => {
     // each test case can get its own call to the test function
     test("empty array", () => {
-        let result = sum([])
+        let result = sum([]);
         // this is a basic assertion, if an assertion fails the whole
         // test fails
         expect(result).toEqual(0);
-    })
+    });
 
-    // more ideas for test cases
     // also test with some positive numbers
+    test("positive numbers", () => {
+        let result = sum([1, 3, 4]);
+        // this is a basic assertion, if an assertion fails the whole
+        // test fails
+        expect(result).toEqual(8);
+    });
+    
+    // more ideas for test cases
+    
     // test with one number
     // test with some negative numbers
-})
+});
